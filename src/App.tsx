e47db7a24b01a3087c23e5a171103b2c9b2fc995
Glenn Vinck <glenn@wearemoghi.be>
2025-10-16 11:52:39 +0200

    feat: add react-router and setup homepage + requestservice page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-import "./App.css";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <div>Ring Twice</div>
+      <main>
+        <h1>Des voisins talentueux, à deux pas de chez vous !</h1>
+        <p>
+          Demandez d'aide et selectionnez la meilleure offre parmi 50.000
+          particuliers et professionnels !
+        </p>
+        <Link to="/requestService">Hulp nodig? Plaats een aanvraag</Link>
+        <Link to="#">Ik bied mijn diensten aan</Link>
+      </main>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+import RequestService from "./views/requestService.tsx";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/requestService",
+    element: <RequestService />,
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
