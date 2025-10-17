8ed0095a301c2dae6576dff3ae933feb9bb55302
Glenn Vinck <glenn@wearemoghi.be>
2025-10-17 10:08:55 +0200

    ref: refactor homepage and file structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
-import Home from "./views/home.tsx";
+import ButtonLink from "./components/ui/ButtonLink.tsx";
 
 function App() {
   return (
     <>
       <header>Header</header>
-      <Home />
+      <main>
+        <section className="px-3 py-4">
+          <div className="w-full aspect-square bg-[#eaddd1] mb-4"></div>
+          <div className="flex flex-col gap-4">
+            <h1 className="font-normal text-3xl">
+              Des voisins talentueux, à deux pas de chez vous !
+            </h1>
+            <p className="opacity-60">
+              Demandez d'aide et selectionnez la meilleure offre parmi 50.000
+              particuliers et professionnels !
+            </p>
+            <div className="flex flex-row flex-wrap gap-3">
+              <ButtonLink to="/request-service">
+                Hulp nodig? Plaats een aanvraag
+              </ButtonLink>
+              <ButtonLink to="#" variant="secondary">
+                Ik bied mijn diensten aan
+              </ButtonLink>
+            </div>
+          </div>
+        </section>
+      </main>
       <footer>Footer</footer>
     </>
   );
